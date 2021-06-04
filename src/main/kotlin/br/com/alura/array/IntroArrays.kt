8b5a0e67f302682f1ce88a26df07fbc3a5a6b1c6
Luiz Moraes <lffm1994@gmail.com>
2021-06-04 11:05:56 -0300

    Modulo 1 - Alterando um elemento

--- src/main/kotlin/br/com/alura/array/IntroArrays.kt
+++ src/main/kotlin/br/com/alura/array/IntroArrays.kt
@@ -1,65 +1,38 @@
 package br.com.alura.array
 
+import br.com.alura.teste.testeMaiorEMenorIdade
+
 /*
- Saber qual é o funcionario mais velho de uma equipe
+    Calcular 10% de aumento no salário de cada um dos funcionários
  */
 fun main(){
-    val idade1 = 25
-    val idade2 = 19
-    val idade3 = 33
-    val idade4 = 20
+//    testeMaiorEMenorIdade()
 
-    val maiorIdade = if(idade1 > idade2 && idade1 > idade3 && idade1 > idade4) {
-        idade1
-    }else if(idade2 > idade3 && idade2 > idade4){
-        idade2
-    }else if(idade3 > idade4) {
-        idade3
-    }else{
-        idade4
+    //Criação de Array tipo Double
+    var salarios: DoubleArray = doubleArrayOf(1500.50, 2300.0, 5000.0, 8000.0, 10000.0)
+
+    val aumento = 1.1
+//    var indice = 0
+//    //For incrementar o indice manualmente
+//    for(salario in salarios){
+//        salarios[indice] = salario * aumento
+//        indice++
+//    }
+//    println("O valor dos salarios após aumentar 10% foi ${salarios.contentToString()}")
+
+    //For sem incrementar o indice manualmente
+    for(indice in salarios.indices){
+        salarios[indice] = salarios[indice] * aumento
+    }
+    //Exibir valores dentro do array contentToString
+    println("O valor dos salarios após aumentar 10% foi ${salarios.contentToString()}")
+
+    //Função forEachIndexed para percorrer e realizar calculo de aumento
+
+    salarios.forEachIndexed { i, salario ->
+        salarios[i] = salario * aumento
     }
 
-    println(maiorIdade)
-
-    /*
-     Utilizando estrutura de dados - Array de Inteiros
-     - e preciso informar o tamanho do array no momento da criação
-     - o indice de Array começa na posição 0
-     - Função para otimizar a criação de Array intArrayOf
-     */
-
-//    Metodo Tradicional
-//    val idades = IntArray(5)
-//    idades[0] = 25
-//    idades[1] = 19
-//    idades[2] = 33
-//    idades[3] = 20
-//    idades[4] = 55
-
-    var idades: IntArray = intArrayOf(25, 19, 33, 20, 55, 40)
-
-    //Utilização de um for para iteração
-
-    var maiorIdadeArray = Int.MIN_VALUE
-    for (idade in idades){
-        if(idade > maiorIdadeArray){
-            maiorIdadeArray = idade
-        }
-    }
-    println("A maior idade do Array é $maiorIdadeArray")
-
-    /*
-    Outro metodo de iteração para descobrir a maior idade ao percorrer o Array
-    - Int.MAX_VALUE é uma contante garanque que começa com o maior valor
-     */
-
-    var menorIdade = Int.MAX_VALUE
-    idades.forEach { idade ->
-        if(idade < menorIdade){
-            menorIdade = idade
-        }
-    }
-
-    println("A menor idade do Array é $menorIdade")
-
+    println("O valor dos salarios após aumentar 10% foi ${salarios.contentToString()}")
 }
+
---
+++ src/main/kotlin/br/com/alura/teste/testeMaiorEMenorIdade.kt
@@ -0,0 +1,64 @@
+package br.com.alura.teste
+
+/*
+ Saber qual é o funcionario mais velho de uma equipe
+ */
+fun testeMaiorEMenorIdade() {
+    val idade1 = 25
+    val idade2 = 19
+    val idade3 = 33
+    val idade4 = 20
+
+    val maiorIdade = if (idade1 > idade2 && idade1 > idade3 && idade1 > idade4) {
+        idade1
+    } else if (idade2 > idade3 && idade2 > idade4) {
+        idade2
+    } else if (idade3 > idade4) {
+        idade3
+    } else {
+        idade4
+    }
+
+    println(maiorIdade)
+
+    /*
+     Utilizando estrutura de dados - Array de Inteiros
+     - e preciso informar o tamanho do array no momento da criação
+     - o indice de Array começa na posição 0
+     - Função para otimizar a criação de Array intArrayOf
+     */
+
+//    Metodo Tradicional
+//    val idades = IntArray(5)
+//    idades[0] = 25
+//    idades[1] = 19
+//    idades[2] = 33
+//    idades[3] = 20
+//    idades[4] = 55
+
+    var idades: IntArray = intArrayOf(25, 19, 33, 20, 55, 40)
+
+    //Utilização de um for para iteração
+
+    var maiorIdadeArray = Int.MIN_VALUE
+    for (idade in idades) {
+        if (idade > maiorIdadeArray) {
+            maiorIdadeArray = idade
+        }
+    }
+    println("A maior idade do Array é $maiorIdadeArray")
+
+    /*
+    Outro metodo de iteração para descobrir a maior idade ao percorrer o Array
+    - Int.MAX_VALUE é uma contante garanque que começa com o maior valor
+     */
+
+    var menorIdade = Int.MAX_VALUE
+    idades.forEach { idade ->
+        if (idade < menorIdade) {
+            menorIdade = idade
+        }
+    }
+
+    println("A menor idade do Array é $menorIdade")
+}
