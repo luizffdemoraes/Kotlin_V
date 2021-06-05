c8d7e28a3a43f675fa2ea0304c1eae9426ace367
Luiz Moraes <lffm1994@gmail.com>
2021-06-05 20:02:35 -0300

    Modulo 4 - Trabalhando com listas

---
+++ src/main/kotlin/br/com/alura/list/ListaDeLicros.kt
@@ -0,0 +1,54 @@
+package br.com.alura.list
+
+fun main() {
+    val livro1 = Livro(
+        titulo = "Grande Sertão: Veredas",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1956
+    )
+
+    val livro2 = Livro(
+        titulo = "Minha vida de menina",
+        autor = "Helena Morley",
+        anoPublicacao = 1942,
+        editora = "Editora A"
+    )
+
+    val livro3 = Livro(
+        titulo = "Memórias Póstumas de Brás Cubas",
+        autor = "Machado de Assis",
+        anoPublicacao = 1881
+    )
+
+    val livro4 = Livro(
+        titulo = "Iracema",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    )
+
+    /*
+        Agrupar os livros em uma lista - Lista trabalha com objetos
+        Assim como no java no kotlin temos generics e uma caracteristica para criar objeto de tipo generico
+        Diferença do List e Array
+        A lista permite utilizar o metodo add pois tem tamanhos dinamicos
+        Array os valores são fixos caso precise aumentar e necessário criar um novo array
+        Agora listas mutaveis permite alteração
+     */
+    val livros: MutableList<Livro> = mutableListOf(livro1, livro2, livro3, livro4)
+
+    livros.add(
+        Livro(
+            titulo = "Sagarana",
+            autor = "João Guimarães Rosa",
+            anoPublicacao = 1946
+        )
+    )
+
+    println(livros)
+
+    livros.remove(livro1)
+
+    println(livros)
+
+}
