3872edcf62dc3a650c48d1ac132ed6d83cdaf9fb
Luiz Moraes <lffm1994@gmail.com>
2021-06-05 01:56:48 -0300

    Modulo 2 - Array de objetos

--- src/main/kotlin/br/com/alura/array/IntroArrays.kt
+++ src/main/kotlin/br/com/alura/array/IntroArrays.kt
@@ -1,40 +1,65 @@
 package br.com.alura.array
 
 import br.com.alura.teste.testaAumentoSalario
+import br.com.alura.teste.testaOperacoesAgregadoras
 import br.com.alura.teste.testeMaiorEMenorIdade
 import br.com.alura.teste.testeRange
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 
 fun main() {
 //    testeMaiorEMenorIdade()
 //    testaAumentoSalario()
 //    testeRange()
+//    testaOperacoesAgregadoras()
 
-    val idades: IntArray = intArrayOf(10,12,18,33,40,67)
-    val maiorIdade = idades.maxOrNull()
-    println("Maior idade: $maiorIdade")
+    /*
+    Todo funcionario tem um aumento de 10%, e no minimo 500 caso o calculo resulte
+     em um valor inferior, utilizaremos o tipo BigDecimal por questões de arredondamento
+     visando garantir exatidão no calculo
+     O bigDeciaml e um tipo primitivo ele não tem uma implementação de array
+     criaremos um objeto e com essa classe criaremos um Array
+     */
 
-    val menorIdade = idades.minOrNull()
-    println("Menor idade: $menorIdade")
+    val salarios = bigDecimalArrayOf("1500.55", "2000.00", "5000.00", "10000.00")
 
-    //saber a media das idades
-    val media: Double = idades.average()
-    println("Média: $media")
+//        Array<BigDecimal>(5){ BigDecimal.ZERO }
+//    salarios[0] = "1500.55".toBigDecimal() //para conversão visando o arredondamento usamos o string
+//    salarios[1] = "2000.00".toBigDecimal()
+    println(salarios.contentToString())
 
-    //verificar se todos são maior de idade
-    val todosMaiores = idades.all { it > 18 }
-    println("Todos maiores? $todosMaiores")
+    /*
+    Manteremos dois array um tera o valor iniciar e outro com valor do aumento
+    utilizaremos a função map para mapear diferente o forEach a função map ela retorna outra
+    lista vamos precisar fazer uma conversão para Array
+     */
 
-    //verificar se pelo menos um e maior de idade
 
-    val existeMaior = idades.any {it >= 18 }
-    println("Algum aluno é maior de idade? $existeMaior")
+    //Estamos pegando salarios mapeando cada salario  * aumento
+    // e convertendo para um Array de volta com aumento
+    //definimos agora com o set a quantidade de valores após a vilgula e o arredondamento
+    //calculo com if para o aumento minimo de R$500
+    val aumento = "1.1".toBigDecimal()
+    val salariosComAumento: Array<BigDecimal> = salarios
+        .map { salario ->
+            if (salario < "5000".toBigDecimal()) {
+                salario + "500.00".toBigDecimal()
+            } else {
+                (salario * aumento).setScale(2, RoundingMode.UP)
+            }
+        }
+        .toTypedArray()
 
-    //filtrar os valores menores de 18 e trazer os maiores ou iguais
-    val maiores = idades.filter { it >= 18 }
-    println("Maiores: $maiores")
+    println(salariosComAumento.contentToString())
 
-    //retornar um valor especifico retorna o primeiro valor que satisfizer a condição
-    val busca = idades.find { it == 18}
-    println("Busca: $busca")
+}
+
+//Criação de Array de forma dinamica
+//vararg podemos passar duas variaveis ou mais
+fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal> {
+    //Essa função esta pebando cada valor pelo indice i e convertendo para Big Decimal
+    return Array<BigDecimal>(valores.size) { i ->
+        valores[i].toBigDecimal()
+    }
 }
---
+++ src/main/kotlin/br/com/alura/teste/testaOperacoesAgregadoras.kt
@@ -0,0 +1,31 @@
+package br.com.alura.teste
+
+fun testaOperacoesAgregadoras() {
+    val idades: IntArray = intArrayOf(10, 12, 18, 33, 40, 67)
+    val maiorIdade = idades.maxOrNull()
+    println("Maior idade: $maiorIdade")
+
+    val menorIdade = idades.minOrNull()
+    println("Menor idade: $menorIdade")
+
+    //saber a media das idades
+    val media: Double = idades.average()
+    println("Média: $media")
+
+    //verificar se todos são maior de idade
+    val todosMaiores = idades.all { it > 18 }
+    println("Todos maiores? $todosMaiores")
+
+    //verificar se pelo menos um e maior de idade
+
+    val existeMaior = idades.any { it >= 18 }
+    println("Algum aluno é maior de idade? $existeMaior")
+
+    //filtrar os valores menores de 18 e trazer os maiores ou iguais
+    val maiores = idades.filter { it >= 18 }
+    println("Maiores: $maiores")
+
+    //retornar um valor especifico retorna o primeiro valor que satisfizer a condição
+    val busca = idades.find { it == 18 }
+    println("Busca: $busca")
+}
